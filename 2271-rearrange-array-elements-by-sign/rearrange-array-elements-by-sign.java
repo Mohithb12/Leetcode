e3e4654857fc824ab9a5825f167bfdb5acbac7ee
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-06-22 08:03:49 +0530

    Time: 3 ms (100.00%) | Memory: 77.4 MB (35.15%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,92 +1,111 @@
 class Solution {
-    public static void rearange(int[] nums){
-        // int cons =  0,arb=1,len = nums.length;
+    // public static int[] rearange(int[] nums){
+    //     // int cons =  0,arb=1,len = nums.length;
         
-        // while(arb< len)
-        // {
-        //     if(cons%2 == 0 && nums[cons]< 0 )
-        //     {
-        //         // System.out.println("even"+cons);
-        //         while(arb<len){
-        //             if(nums[arb]>=0)
-        //             {
-        //                 int temp = nums[arb];
-        //                 nums[arb] = nums[cons];
-        //                 nums[cons]  = temp;
+    //     // while(arb< len)
+    //     // {
+    //     //     if(cons%2 == 0 && nums[cons]< 0 )
+    //     //     {
+    //     //         // System.out.println("even"+cons);
+    //     //         while(arb<len){
+    //     //             if(nums[arb]>=0)
+    //     //             {
+    //     //                 int temp = nums[arb];
+    //     //                 nums[arb] = nums[cons];
+    //     //                 nums[cons]  = temp;
 
-        //                 ++ cons;
-        //                 arb = cons+1;
-        //                 break;
-        //             }
-        //             else
-        //                 arb++;
+    //     //                 ++ cons;
+    //     //                 arb = cons+1;
+    //     //                 break;
+    //     //             }
+    //     //             else
+    //     //                 arb++;
                       
-        //         }
-        //     }
-        //     // else if(cons%2 == 0 && nums[cons]>= 0 )
-        //     // {
+    //     //         }
+    //     //     }
+    //     //     // else if(cons%2 == 0 && nums[cons]>= 0 )
+    //     //     // {
                 
-        //     // }
+    //     //     // }
 
-        //     else if(cons%2 == 1 && nums[cons] >= 0 )
+    //     //     else if(cons%2 == 1 && nums[cons] >= 0 )
             
-        //     {   
-        //         // System.out.println("odd"+cons);
-        //         while(arb<len){
-        //             if(nums[arb]<0)
-        //             {
-        //                 int temp = nums[arb];
-        //                 nums[arb] = nums[cons];
-        //                 nums[cons]  = temp;
+    //     //     {   
+    //     //         // System.out.println("odd"+cons);
+    //     //         while(arb<len){
+    //     //             if(nums[arb]<0)
+    //     //             {
+    //     //                 int temp = nums[arb];
+    //     //                 nums[arb] = nums[cons];
+    //     //                 nums[cons]  = temp;
 
-        //                 ++ cons;
-        //                 arb = cons+1;
-        //                 break;
-        //             }
-        //             else
-        //                 arb++;
+    //     //                 ++ cons;
+    //     //                 arb = cons+1;
+    //     //                 break;
+    //     //             }
+    //     //             else
+    //     //                 arb++;
                       
-        //         }
-        //     }
+    //     //         }
+    //     //     }
 
-        //     else
-        //     {
-        //         cons ++;
-        //         arb = cons+1;
-        //     }
+    //     //     else
+    //     //     {
+    //     //         cons ++;
+    //     //         arb = cons+1;
+    //     //     }
 
-        // }
+    //     // }
 
-        int n = nums.length,j=0,k=0;
+    //     int n = nums.length,j=0,k=0;
+    //     int[] result = new int[n];
 
-        int pos[] = new int[n/2];
-        int neg[] = new int[n/2];
-
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i] > 0)
-                pos[j++] = nums[i];
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         if(nums[i] > 0)
+    //         {    
+    //             result[j] = nums[i];
+    //             j +=2;
+               
+    //         }    
             
-            if(nums[i] < 0)
-                neg[k++] = nums[i];
-            
-        }
+    //         if(nums[i] < 0)
+    //         {
+    //             result[k] = nums[i];
+    //             k +=2;
+    //         }
+    //     }
 
-        for(int i=0;i<n/2;i++)
-        {
-            nums[2*i] = pos[i];
-            nums[2*i+1] = neg[i];
-        }
+        
+    //     return result;
 
-
-    }
+    // }
 
 
     public int[] rearrangeArray(int[] nums) {
 
-        rearange(nums);
+        int n = nums.length,j=0,k=1;
+        int[] result = new int[n];
 
-        return nums;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] > 0)
+            {    
+                result[j] = nums[i];
+                j +=2;
+               
+            }    
+            
+            if(nums[i] < 0)
+            {
+                result[k] = nums[i];
+                k +=2;
+            }
+        }
+        
+        return result;
+
+        
 
     }
 }
