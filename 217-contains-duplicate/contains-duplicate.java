10122d744d60e4be2b68f4655cec6e4229ea144b
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-06-23 22:10:10 +0530

    Time: 13 ms (87.44%) | Memory: 57.8 MB (88.78%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,25 +1,38 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        HashMap<Integer,Integer> maps = new HashMap<>();
+        // HashMap<Integer,Integer> maps = new HashMap<>();
         int n = nums.length;
-        for(int i=0;i<n;i++)
-        {
-            if(maps.containsKey(nums[i]))
-            {   
-                int value = maps.get(nums[i]);
-                maps.put(nums[i],value+1);
-            }
-            else
-            {
-                maps.put(nums[i],0);
-            }
-        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(maps.containsKey(nums[i]))
+        //     {   
+        //         int value = maps.get(nums[i]);
+        //         maps.put(nums[i],value+1);
+        //     }
+        //     else
+        //     {
+        //         maps.put(nums[i],0);
+        //     }
+        // }
 
-        for(int i=0;i<n;i++)
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(maps.get(nums[i]) >0 )
+        //         return true;
+        // }
+
+        // return false;
+
+        Set<Integer> setstore = new HashSet<>();
+
+        for(int number:nums)
         {
-            if(maps.get(nums[i]) >0 )
+            if(setstore.contains(number))
                 return true;
+
+            setstore.add(number);
+            
         }
 
         return false;
