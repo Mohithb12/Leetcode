51816e732c12705a2a7bc42439b807a1b3fefd67
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-07-15 22:24:39 +0530

    Time: 1 ms (72.29%) | Memory: 45.6 MB (64.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -45,6 +45,7 @@ class Solution {
 
         // Better using 2 arrays
 
+/*
         int[] zero_row = new int[row] ;
 
         int[] zero_col = new int[col];
@@ -71,5 +72,52 @@ class Solution {
                 }
             }
         }
+        */
+
+        // optimal
+
+        int col0 =1;
+        for(int m =0;m<row;m++)
+        {
+            for(int n=0;n<col;n++)
+            {
+                if(matrix[m][n] == 0)
+                {
+                    matrix[m][0] = 0;
+                    
+                    if(n != 0)
+                        matrix[0][n] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for(int m=1 ; m<row ; m++)
+        {
+            for(int n=1; n<col;n++)
+            {
+                if(matrix[0][n] == 0 || matrix[m][0]==0)
+                    matrix[m][n] = 0;
+            }
+        }
+
+        if(matrix[0][0] == 0)
+        {
+            for(int i = 0;i<col;i++)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(col0 == 0)
+        {
+            for(int i=0;i<row;i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+
     }
 }
