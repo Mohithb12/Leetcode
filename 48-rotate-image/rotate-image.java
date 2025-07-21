737acdca0f02f587fdea1ecdbd787fa06b615810
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-07-21 16:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (77.12%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -3,6 +3,9 @@ class Solution {
         
         
         int n = matrix.length;
+        // brute force
+
+        /*
         int[][] res = new int[n][n];
 
         for(int i=0;i<n;i++)
@@ -19,7 +22,34 @@ class Solution {
                 matrix[i][j] = res[i][j];
             }
         }
-        
+        */
+
+        // in space
+        int temp , l ,r;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i > j)
+                    continue;
+                temp = matrix[i][j] ;
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] =  temp;
+            }
+            l =0 ; 
+            r =n-1;
+             
+            while(l <= r){
+                temp = matrix[i][l] ;
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+                l++;
+                r--;
+            }
+            
+        }
+
+
         
         
     }
