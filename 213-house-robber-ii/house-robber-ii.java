2928fc7cadbb71ad50b2c33cc270c100583af410
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-08-04 19:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (64.22%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        
+        // Exclude last house
+        int case1 = robLinear(nums, 0, nums.length - 2);
+        // Exclude first house
+        int case2 = robLinear(nums, 1, nums.length - 1);
+        
+        return Math.max(case1, case2);
+    }
+    
+    private int robLinear(int[] nums, int start, int end) {
+        int prev = 0, curr = 0;
+        for (int i = start; i <= end; i++) {
+            int temp = Math.max(curr, prev + nums[i]);
+            prev = curr;
+            curr = temp;
+        }
+        return curr;
+    }
+}
