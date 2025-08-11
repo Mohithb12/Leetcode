b68b2dbd040aa71136a1a928312b7d572d0b58ab
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-08-11 23:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (91.11%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,22 +1,29 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int notleave = maxProfit(nums,0,n-2);
+        int leave = maxProfit(nums,1,n-1);
+
+        return Math.max(leave,notleave);
+
         
-        // Exclude last house
-        int case1 = robLinear(nums, 0, nums.length - 2);
-        // Exclude first house
-        int case2 = robLinear(nums, 1, nums.length - 1);
-        
-        return Math.max(case1, case2);
     }
-    
-    private int robLinear(int[] nums, int start, int end) {
-        int prev = 0, curr = 0;
-        for (int i = start; i <= end; i++) {
-            int temp = Math.max(curr, prev + nums[i]);
-            prev = curr;
-            curr = temp;
+
+    static int maxProfit(int[] nums,int start,int end)
+    {
+        int prev=0,prev2=0;
+
+        
+
+        for(;start<=end;start++)
+        {
+            int cur = Math.max(prev,prev2+nums[start]);
+
+            prev2 = prev ;
+            prev = cur;
         }
-        return curr;
+
+        return prev;
     }
 }
