7e3c007a0d70246bf38acb652a5c337dd6b7c332
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-08-28 21:53:44 +0530

    Time: 26 ms (29.97%) | Memory: 45.7 MB (12.35%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,38 +1,36 @@
 class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp =new int[coins.length][amount+1];
 
-    static int minCoins(int ind,int amount,int[] coins,HashMap<String,Integer> map) {
-
-        if(ind == 0){
-            if(amount % coins[0] == 0)
-                return amount/ coins[0];
-            
-            return (int) 1e9;
+        for(int[] rows:dp)
+        {
+            Arrays.fill(rows,-1);
         }
 
-        String key = ind + "-" + amount;
+        int res=fewest(coins,coins.length -1,amount,dp);
+        return res >= (int)1e9 ? -1 : res;
 
-        if(!map.containsKey(key)){
-        int left = (int) 1e9;
-        if(coins[ind] <= amount)
-             left = 1 + minCoins(ind,amount - coins[ind],coins,map);
-
-        int right = minCoins(ind-1,amount,coins,map);
-
-        map.put(key,Math.min(left,right));
-
-        }
-
-        return map.get(key); 
     }
 
-    public int coinChange(int[] coins, int amount) {
+    static int fewest(int[] coins,int ind,int amount,int[][] dp)
+    {
+        if(ind == 0){
+            if( amount % coins[0] == 0) 
+                return amount/coins[0];
+
+            return (int)1e9;
+        }
+
+        if(dp[ind][amount] != -1) return dp[ind][amount];
         
-        HashMap<String,Integer> map = new HashMap<>();
+        int take=(int)1e9;
+        if(coins[ind] <= amount)
+            take = 1 + fewest(coins,ind,amount - coins[ind],dp);
+
+        int nottake = fewest(coins,ind-1,amount,dp);
+
+        return dp[ind][amount]=Math.min(take,nottake);
+
         
-        int ans = minCoins(coins.length-1,amount,coins,map);
-
-        return (ans >= (int) 1e9) ? -1 : ans;
-
-
     }
 }
