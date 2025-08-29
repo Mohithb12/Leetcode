781bf7bc200e4459ce352742c326dd59d5bb67db
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-08-29 20:34:29 +0530

    Time: 111 ms (5.15%) | Memory: 44.8 MB (43.88%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,54 +1,43 @@
 class Solution {
-    // public int coinChange(int[] coins, int amount) {
-    //     int[][] dp =new int[coins.length][amount+1];
-
-    //     for(int[] rows:dp)
-    //     {
-    //         Arrays.fill(rows,-1);
-    //     }
-
-    //     int res=fewest(coins,coins.length -1,amount,dp);
-    //     return res >= (int)1e9 ? -1 : res;
-
-    // }
-
-      public  static int coinChange(int[] arr, int T) {
+       public static int coinChange(int[] arr, int T) {
         int n = arr.length;
 
-        // Create a 2D array to store results of subproblems
-        int dp[][] = new int[n][T + 1];
+        // Create two arrays to store results of subproblems: prev and cur
+        int prev[] = new int[T + 1];
+        int cur[] = new int[T + 1];
 
-        // Initialize the dp array for the first element of the array
+        // Initialize the prev array for the first element of the array
         for (int i = 0; i <= T; i++) {
             if (i % arr[0] == 0)
-                dp[0][i] = i / arr[0];
+                prev[i] = i / arr[0];
             else
-                dp[0][i] = (int) Math.pow(10, 9);
+                prev[i] = (int) Math.pow(10, 9);
         }
 
-        // Fill the dp array using dynamic programming
+        // Fill the cur array using dynamic programming
         for (int ind = 1; ind < n; ind++) {
             for (int target = 0; target <= T; target++) {
-                int notTake = 0 + dp[ind - 1][target];
+                int notTake = 0 + prev[target];
                 int take = (int) Math.pow(10, 9);
 
                 // If the current element is less than or equal to the target, calculate 'take'
                 if (arr[ind] <= target)
-                    take = 1 + dp[ind][target - arr[ind]];
+                    take = 1 + cur[target - arr[ind]];
 
-                // Store the minimum result in the dp array
-                dp[ind][target] = Math.min(notTake, take);
+                // Store the minimum result in the cur array
+                cur[target] = Math.min(notTake, take);
             }
+
+            // Update prev with cur for the next iteration
+            prev = cur.clone();
         }
 
         // Get the minimum number of elements needed for the target sum
-        int ans = dp[n-1][T];
-        
+        int ans = prev[T];
 
         // If it's not possible to achieve the target sum, return -1
         if (ans >= (int) Math.pow(10, 9))
             return -1;
         return ans;
-      }
-    
+    }
 }
