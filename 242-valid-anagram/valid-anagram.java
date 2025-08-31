7aa15a8d39b8c5fdf98f42173f69646e37b6415e
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-08-31 12:28:52 +0530

    Time: 5 ms (56.11%) | Memory: 42.9 MB (88.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,16 +3,19 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] count=new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(count[i]!=0){
-                return false;
-            }
-        }
+
+        int arr[] = new int[26];
+
+        for(int i=0;i<s.length();i++)
+        {
+            arr[s.charAt(i) - 'a'] ++;
+            arr[t.charAt(i) - 'a'] --;
+        } 
+
+        for(int num : arr)
+            if(num != 0) return false;
+        
+
         return true;
-    }
+     }
 }
