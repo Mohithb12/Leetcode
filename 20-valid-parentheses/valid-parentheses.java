5363eccb0f6036bea67ccbd07c32ec839e12299f
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-09-01 21:34:16 +0530

    Time: 1 ms (99.47%) | Memory: 41.7 MB (68.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-       
+        if( s.length() == 1) return false;
         char[] stack = new char[s.length()];
         int top = -1;
 
         for(char ch : s.toCharArray()){
-            if(ch == '('   )
-                stack[++top] = ')';
-            else if(ch == '{' )
-                stack[++top] ='}';
-            else if(ch == '[')
-                stack[++top] = ']';
-            else if(top == -1 || stack[top--]!=ch)
+            if(ch == '(' ||ch == '{' || ch == '['){
+                top ++;
+                stack[top] = ch;
+                
+            }
+            else if (top ==-1)
+                return false;
+            else if(ch == ')' && stack[top]=='(' )
+                top--;
+            else if(ch == '}' && stack[top]=='{' )
+                top--;
+            else if(ch == ']' && stack[top]=='[' )
+                top--;
+            else 
                 return false;
             
         }
-        if(top == -1)
-            return true;
-        else 
-        return false;
+        return top == -1 ? true : false;
 
     }
 }
