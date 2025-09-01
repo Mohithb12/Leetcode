79c7db61637140edb9a62cba68c5b8328ff9fc4f
Mohithb12 <133988964+Mohithb12@users.noreply.github.com>
2025-09-01 22:22:46 +0530

    Time: 6 ms (3.29%) | Memory: 44.6 MB (5.75%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sqrsm = 0;
+        // if(n > 1 && n <10)
+        //     return false;
+        int[] hash = new int[100000];
+
+        while(sqrsm != 1){
+            sqrsm =0;
+
+            while(n != 0)
+            {
+                sqrsm += Math.pow(n%10,2);
+                n /= 10;
+            }
+            // System.out.println(sqrsm);
+            if(hash[sqrsm] == 1)
+                return false;
+            else
+                hash[sqrsm]=1;
+            n = sqrsm;
+        }
+
+        return true;
+    }
+}
